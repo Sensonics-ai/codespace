de3f2d0f04366b34bb2fb34fae9044f3a51a32cd
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-03-09 13:33:52 +0000

    Thu, Mar 9, 2023, 1:33 PM +00:00

--- hello.c
+++ hello.c
@@ -8,7 +8,7 @@ int main(void)
     string first = get_string("what's your first name? ");
     string last = get_string("what's your last name? ");
     printf("hello, %s %s\n", first, last);
-
+    //each %s just basically calls and individual variable. you need as much as variables you are gonna have.
     //note that "string" basically is telling the program that I want to get a "line of text" out of this. First and Last are just Wwhat Im telling the program that is going to be my answers (first = my first name, last= my last name)
 }
 // the "\n" stands for "New line". so the terminal will create a NEW LINE after running the program. It makes everything clearer. it's an Escape Sequence.
