14ddd3da3871f61123247c380e2b1032b4d4c3d7
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-03-09 09:57:07 +0000

    Thu, Mar 9, 2023, 9:57 AM +00:00

--- hello.c
+++ hello.c
@@ -1,6 +1,8 @@
+//header (library), its calling for the function "printf" to exist. it's not gonna know what printf is if we dont include this particular library of C
 #include <stdio.h>
 
 int main(void)
 {
     printf("hello, world\n");
 }
+//finally working !!
