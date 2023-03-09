5a9e3b6ee34bcdaab85b16c7fbbd196254f66f73
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-03-09 13:18:43 +0000

    Thu, Mar 9, 2023, 1:18 PM +00:00

--- hello.c
+++ hello.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 //header files (library), its calling for the function "printf" to exist. it's not gonna know what printf is if we dont import/include this particular library of C. it's like adding the voice lines library in Scratch.
+#include <cs50.h>
+
 
 int main(void)
 {
+    string answer = get_string("what's your name? ");
     printf("hello, world\n");
 }
 // the "\n" stands for "New line". so the terminal will create a NEW LINE after running the program. It makes everything clearer. it's an Escape Sequence.
