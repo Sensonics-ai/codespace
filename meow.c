263899a9afd46029c37fb87d2875c879636a9b74
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-03-14 12:51:50 +0000

    Tue, Mar 14, 2023, 12:51 PM +00:00

--- meow.c
+++ meow.c
@@ -7,4 +7,5 @@ int main(void)
     printf("meow\n");
 
 //you can highlight the lines clicking on line number and with "Shift + L" you click the rest.
+//lets say you wanted to change one of those meows for a woof. how could you do that? easy, varibales:
 }
