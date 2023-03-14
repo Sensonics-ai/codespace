3baf3b772cfeeaf73c631e444543bbbb0cc372e3
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-03-14 10:35:58 +0000

    Tue, Mar 14, 2023, 10:35 AM +00:00

--- agree.c
+++ agree.c
@@ -8,9 +8,14 @@ int main(void)
     char c = get_char("Do you agree?\n");
 
     if (c == 'y')
+
     {
         printf("Agreed.\n");
-
+    }
+    //notice that  y is not in CAPS, that matters when you want to answer. You could add the following conditions in additional "elses" like the following:
+    else if (c == 'Y')
+    {
+        printf("Agreed.\n");
     }
     else if (c == 'n')
     {
