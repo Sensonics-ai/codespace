0d7e6d4dc33048b8ad51f6a3d7281ea582649857
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-03-14 12:13:16 +0000

    Tue, Mar 14, 2023, 12:13 PM +00:00

--- meow.c
+++ meow.c
@@ -5,4 +5,6 @@ int main(void)
     printf("meow\n");
     printf("meow\n");
     printf("meow\n");
+
+//you can highlight the lines clicking on line number and with "Shift + L" you click the rest.
 }
