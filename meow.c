126863c56f2921c71e121eae10a82e37285bdc9d
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-03-14 13:05:12 +0000

    Tue, Mar 14, 2023, 1:05 PM +00:00

--- meow.c
+++ meow.c
@@ -8,4 +8,17 @@ int main(void)
 
 //you can highlight the lines clicking on line number and with "ctrl + Shift + L" you click the rest.
 //lets say you wanted to change one of those meows for a woof. how could you do that? easy, varibales:
+
+int counter = 0;
+
+//if you want to increment by 1;
+counter = counter + 1;
+
+// that just assigning counter current value + 1 to create the new counter value.
+//Note that you dont need to add "Int" because counter already exists. You only need "int" the first time you create something.
+//Alternatively you can write the line 15 differently;
+
+counter += 1;
+
+
 }
