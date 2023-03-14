4b6185a5b1a3029ee16df3fe405d414600396b20
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-03-14 13:37:47 +0000

    Tue, Mar 14, 2023, 1:37 PM +00:00

--- meow2.c
+++ meow2.c
@@ -5,5 +5,16 @@ while (counter > 0)
 
 {
     printf("meow\n");
-    counter = counter -1
+    counter = counter -1;
 }
+
+//We can also write the same thing differently, for the sake of simplicity;
+
+int i = 3
+while (i > 0)
+{
+    printf("meow\n");
+    i = i -1;
+}
+
+//since "i" its just a shorcut for "integer"
