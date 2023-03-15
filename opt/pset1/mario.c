5c48e14e9996b2b62fcc08bac77e90172bee04a5
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-03-15 11:53:02 +0000

    Wed, Mar 15, 2023, 11:53 AM +00:00

--- opt/pset1/mario.c
+++ opt/pset1/mario.c
@@ -2,11 +2,13 @@
 
 int main(void)
 {
-    for (int i = 1; i <= 3; i++)
+    const int n = 5;
+    for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <= 3; j++)
+        for (int j = 1; j <= n; j++)
         {
              printf("#");
+
         }
         printf("\n");
     }
