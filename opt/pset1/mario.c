3043c936e636d887a32b86020b5ca28d9924ffe8
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-03-15 14:14:03 +0000

    Wed, Mar 15, 2023, 2:14 PM +00:00

--- opt/pset1/mario.c
+++ opt/pset1/mario.c
@@ -1,25 +1,42 @@
+//include the library of functions that I will need.
 #include <stdio.h>
 #include <cs50.h>
 
+int get_size(void);
+void print_grid(int size);
 
 int main(void)
 {
+    //get size of brick
+    int n =get_size(); //note how the parenthesis is empty
+
+    //print grid of bricks with the specified size
+    print_grid(n);
+}
+
+//abstraction 1
+int get_size(void)
+//"int" is the output format, "get_size" the name of the function, and "void" the inputs that the function requires, in this case "void", means no inputs are required.
+{
+//get the value n from the user.
      int n;
      do
      {
      n = get_int("Size: ");
      }
      while (n < 1)
-//we are telling the compile to do the following: get int n from the user and store that value in n, while checking that the value n matches our requirements, in this case, to be an Int.
+     return n; //hand it back as a return value into the empty parentesis from line 8
+}
 
-
-    for (int i = 1; i <= n; i++)
+//abstraction 2
+void print_grid(int size) //it has no output, hence why we have void at the beginning, the name of the function print_grid and finally the type and name of input inside the brackets
+{
+    for (int i = 1; i <= size; i++)
     {
-        for (int j = 1; j <= n; j++)
-        {
-             printf("#");
-
-        }
-        printf("\n");
+         for (int j = 1; j <= size; j++)
+         {
+              printf("#");
+         }
+         printf("\n");
     }
 }
