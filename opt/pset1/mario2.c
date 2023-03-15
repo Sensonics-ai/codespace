a9b892b6c4891e6545845c1f1b355e44ec60ce31
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-03-15 11:17:37 +0000

    Wed, Mar 15, 2023, 11:17 AM +00:00

---
+++ opt/pset1/mario2.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+    for (int i = 1; i <= 3; i++)
+    {
+        printf("?");
+
+    }
+    printf("\n");
+}
+//this code will print 3 "?" in a single row of 3 columns.
+//but What if I wanted to 
