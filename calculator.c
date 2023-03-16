536f7e4145032ab23455b1237f827d4e8b47c498
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-03-16 11:26:33 +0000

    Thu, Mar 16, 2023, 11:26 AM +00:00

--- calculator.c
+++ calculator.c
@@ -1,11 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void)
+long main(void)
 {
-   int x = get_int("x: ");
-   int y = get_int("y: ");
+   long x = get_long("x: ");
+   long y = get_long("y: ");
 
-   printf("%i\n", x + y);
+   printf("%li\n", x + y);
+
+//we need to change the printf function in order to recognize the new data type. There're many different ones; %c, %f, %i (integer), %li (long integer), %s (string).
 
 }
