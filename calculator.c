f9f70d0b2712e685c86f8421b9b2dfcfa0d11785
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-03-16 12:02:06 +0000

    Thu, Mar 16, 2023, 12:02 PM +00:00

--- calculator.c
+++ calculator.c
@@ -6,8 +6,8 @@ int main(void)
    long x = get_long("x: ");
    long y = get_long("y: ");
 
-   printf("%li\n", x + y);
-
-//we need to change the printf function in order to recognize the new data type. There're many different ones; %c, %f, %i (integer), %li (long integer), %s (string).
+   float z = (float) x /(float) y;
+   //we need to transform the long x and y into floating points in between long numbers. Otherwise it won't work, since the program still will treat x and y as Long.
+   printf("%f\n", z);
 
 }
