6f4ed79d2f6fa7f45f603249c5df1126cf0110e8
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-03-16 12:12:07 +0000

    Thu, Mar 16, 2023, 12:12 PM +00:00

--- calculator.c
+++ calculator.c
@@ -8,6 +8,6 @@ int main(void)
 
    float z = (float) x /(float) y;
    //we need to transform the long x and y into floating points in between long numbers. Otherwise it won't work, since the program still will treat x and y as Long.
-   printf("%f\n", z);
-
+   printf("%.8f\n", z);
+   //we can define the amount of decimals we want to see if we add the %.xf in the printf function, where x is the desired amount of decimals.
 }
