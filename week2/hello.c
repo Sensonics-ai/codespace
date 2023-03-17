4cf8704ba38b3da6ffd674b95ced5d7de41018df
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-03-17 09:03:10 +0000

    Fri, Mar 17, 2023, 9:03 AM +00:00

--- week2/hello.c
+++ week2/hello.c
@@ -5,5 +5,5 @@ int main(void)
     printf("hello, world\n");
 }
 
-//clang is basically (C languaje). Take note on how its basically doing what make was doing for us. C lang is able to take "command line arguments".
-//It's just an additional word that modifies or specifies a bit that command
+//clang (stands for C languaje). Take note on how its basically doing what make was doing for us. C lang is able to take "command line arguments".
+//It's just an additional word that modifies or specifies a bit that command.
