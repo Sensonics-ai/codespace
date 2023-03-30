b63b6726a295e599e485e60afde89e2e50a39da5
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-03-30 13:34:53 +0000

    Thu, Mar 30, 2023, 1:34 PM +00:00

--- week2/hello.c
+++ week2/hello.c
@@ -7,10 +7,8 @@ int main(void)
 
       string name = get_string("What is your name?\n");
       printf("hello, %s\n", name);
-      
+
 
 }
 
 
-//it will create a program named hello.c
-//"-o" stands for output and it's basically telling the compiler to rename the program to hello.
