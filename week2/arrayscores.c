5c78fc9a9ecf7d4b5aa3541135561bb54c5ec8b4
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-04-05 07:32:48 +0000

    Wed, Apr 5, 2023, 7:32 AM +00:00

--- week2/arrayscores.c
+++ week2/arrayscores.c
@@ -6,4 +6,5 @@ int main(void)
     scores[0] = 72;
     scores[1] = 73;
     scores[2] = 33;
-    printf("Average: %i \n", (scores[0] + scores[1] + scores[2]) / 3);
+    printf("Average: %f \n", (scores[0] + scores[1] + scores[2]) / (float) 3);
+}
