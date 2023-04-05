3cd60f073f53ac237e3a716d0ea217f05febfb0e
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-04-05 09:00:16 +0000

    Wed, Apr 5, 2023, 9:00 AM +00:00

--- week2/hi.c
+++ week2/hi.c
@@ -2,10 +2,7 @@
 
 int main(void)
 {
-    char c1 = 'H';  //this is a character, it is a single letter, or a single symbol
-    char c2 = 'i';
-    char c3 = '!';
-    char c4 = '\0'; //this is the null character, it is used to indicate the end of a string
+   string s = "Hi!";
+   printf("%s\", s);
 
-    printf("%c%c%c%c\n", c1, c2, c3, c4); //this is how you print a string, you print each character individually, and then you print the null character at the end
 }
