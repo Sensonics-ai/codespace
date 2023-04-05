d1596c2cd2c3343c4d51a21301159771634401ed
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-04-05 07:40:55 +0000

    Wed, Apr 5, 2023, 7:40 AM +00:00

--- week2/arrayscores.c
+++ week2/arrayscores.c
@@ -8,3 +8,4 @@ int main(void)
     scores[2] = 33;
     printf("Average: %f \n", (scores[0] + scores[1] + scores[2]) / (float) 3);
 }
+//this function will calculate the medium number of the 3 scores. Note that for it to show me decimals, i had to call the %float, and put the (float) before the divide
