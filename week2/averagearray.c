e87fe6eb6e38f458d0ead7b8b80869ef59ec09b6
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-04-05 08:18:24 +0000

    Wed, Apr 5, 2023, 8:18 AM +00:00

--- week2/averagearray.c
+++ week2/averagearray.c
@@ -3,14 +3,16 @@
 
 float average(int array[]);
 //this is the function prototype, it tells the compiler that there is a function called average, that takes an array of integers as an argument, and returns a float
+
+int N = 3;  //this is a global variable, it is declared outside of the main function, and can be used anywhere in the program
 int main(void)
 {
-    int scores[3];
-    for (int i = 0; i < 3; i++)
+    int scores[N];  //this is a local variable, it is declared inside the main function, and can only be used inside the main function
+    for (int i = 0; i < N; i++)
     {
         scores[i] = get_int("Score: ");
     }
-    printf("Average: %f \n", average(scores, 3));
+    printf("Average: %f \n", average(scores, N));   //this is how you call a function, you pass the arguments in the same order as they are declared in the function prototype
 }
 
 float average(int array[])
