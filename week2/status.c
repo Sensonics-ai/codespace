dab46fdb8f3d8e78aa80cd7cebb84974b827a5a1
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-04-05 10:38:39 +0000

    Wed, Apr 5, 2023, 10:38 AM +00:00

--- week2/status.c
+++ week2/status.c
@@ -5,6 +5,13 @@
     {
         if (argc  != 2)
         {
-            printf("missing command-line argument\n");
-            return 1;
+            printf("missing command-line argument\n");  //this is how you print an error message
+            return 1;   //this is how you return an error code
         }
+        else
+        {
+            printf("hello, %s\n", argv[1]);
+            return 0;       //this is how you return a success code
+
+        }
+    }
