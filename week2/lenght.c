51800a2d41e4bc6755d969ebaec3970e7bc5de84
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-04-05 09:38:20 +0000

    Wed, Apr 5, 2023, 9:38 AM +00:00

--- week2/lenght.c
+++ week2/lenght.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
-#include <cs50.h>
+#include <stdio.h>     
+#include <cs50.h>   //this is the library that contains the get_string function
+#include <string.h> //this is the library that contains the strlen function
 
 int main (void)
 {
     string name = get_string("What is your name?\n");
+    int n = strlen(name);   //this is how you get the length of a string
+    printf("%i\n", n);  //this will print the length of the string
 
-    int n = 0;
-    while (name[n] != '\0')     //this is the same as name[n] != 0
-    {
-        n++;
-    }
-    printf ("%i\n", n); //this will print the lenght of the string
 }
+
+
