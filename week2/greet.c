1f7680d50ea50d917445ecb0f2606d356e0a4d21
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-04-05 10:11:46 +0000

    Wed, Apr 5, 2023, 10:11 AM +00:00

--- week2/greet.c
+++ week2/greet.c
@@ -1,8 +1,18 @@
 #include    <stdio.h>
 #include    <cs50.h>
 
-int main(void)
+int main(int argc, string argv[]) //now the main function has two arguments: argc and argv
+{
+    if (argc == 2)  //this is how you check if the user has typed in a command line argument
+    {
+        printf("hello, %s\n", argv[1]); //this is how you print the command line argument
+    }
+    else
+    {
+        printf("hello, world\n");
+    }
+}
 {
     string name = get_string("What is your name?\n");
     printf("hello, %s\n", name);
-}   
+}
