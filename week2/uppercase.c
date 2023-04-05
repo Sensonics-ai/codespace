3160abe30cea73d96f7ef0b187d4c56c2ce04835
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-04-05 09:41:57 +0000

    Wed, Apr 5, 2023, 9:41 AM +00:00

---
+++ week2/uppercase.c
@@ -0,0 +1,16 @@
+//I'm going to write a code that asks the user for a string, and then prints the string in uppercase
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+    string s = get_string("Before: ");
+    printf("After: ");
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+        printf("%c", toupper(s[i]));
+    }
+    printf("\n");
+}
