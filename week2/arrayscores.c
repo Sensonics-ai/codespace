39917102bf9bc2e50926b8ed48e49cd191e43f5b
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-04-05 08:03:20 +0000

    Wed, Apr 5, 2023, 8:03 AM +00:00

--- week2/arrayscores.c
+++ week2/arrayscores.c
@@ -8,10 +8,10 @@
 int main(void)
 {
     int scores[3];
-    scores[0] = get_int("Score 1: ");
-    scores[1] = get_int("Score 2: ");
-    scores[2] = get_int("Score 3: ");
-
+    for (int i = 0; i < 3; i++)
+    {
+        scores[i] = get_int("Score: ");
+    }
     printf("Average: %f \n", (scores[0] + scores[1] + scores[2]) / (float) 3);
 }
 //this function will ask the user for scores 1, 2 and 3 and then  proceed top calculate the medium number of the 3 scores.
