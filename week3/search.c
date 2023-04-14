3b228369ea6ba1d77582cb5d7003b7041fc06657
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-04-14 09:52:30 +0000

    Fri, Apr 14, 2023, 9:52 AM +00:00

--- week3/search.c
+++ week3/search.c
@@ -1,1 +1,12 @@
-#include 
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Get line of text
+    string s = GetString();
+
+    // Search for \0 (NULL) character
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        printf("%c
