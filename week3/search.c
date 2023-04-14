e801db7d8c116453de45355063bb1ced0b98a8cf
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-04-14 09:57:41 +0000

    Fri, Apr 14, 2023, 9:57 AM +00:00

--- week3/search.c
+++ week3/search.c
@@ -1,12 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
 
+//lets setup a linear search
+
 int main(void)
 {
-    // Get line of text
-    string s = GetString();
 
-    // Search for \0 (NULL) character
-    for (int i = 0; s[i] != '\0'; i++)
+    int numbers[] = {20, 500, 10, 5, 100, 1, 50};
+//we have 7 numbers in the array, so we need to put 7 in the for loop
+    int n = get_int("Number: ");     //get the number from the user
+    for (int i = 0; i < 7; i++)
     {
-        printf("%c
+        if (n == numbers[i])    //if the number is equal to the number in the array
+        {
+            printf("Found\n");
+            return 0;
+
+        }
+    }
+    printf("Not found\n");  
+    return 1;
+}
