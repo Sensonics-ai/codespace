f7aa753d8addfa1d9c99a436e7286c7502032342
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-04-17 09:11:56 +0000

    Mon, Apr 17, 2023, 9:11 AM +00:00

--- week3/phonebook.c
+++ week3/phonebook.c
@@ -1,9 +1,24 @@
-#include <cs50.h>
-#include <stdio.h>
-#include <string.h>
+#include <cs50.h>   //we can get intput from the user
+#include <stdio.h>  //we can get output to the user
+#include <string.h>  //we can compare strings
 
-int main(void)
+int main(void) //int main its good since we don't need command line arguments
 
 {
-    string names[] = {"EMMA", "RODRIGO", "BRIAN", "DAVID"};
+    string names[] = {"Carter", "David"};   //create an array of strings "names"
+    string numbers[] = {"617-555-0100", "617-555-0101"};   //create an array of "strings numbers" that relates to the names
+
+    string name = get_string("Name: ");
+    for (int i = 0; i < 2; i++)
+    {
+        if (strcmp(names [i], name) == 0)
+        {
+            printf("Found %s\n", numbers [i]);
+            return 0;
+
+        }
+    }
+    printf("Not found\n");
+    return 1;
+
 }
