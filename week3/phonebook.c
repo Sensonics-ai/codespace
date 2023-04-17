73ce7b51207d104eb45ca41dd1183b48b0c01402
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-04-17 09:14:46 +0000

    Mon, Apr 17, 2023, 9:14 AM +00:00

--- week3/phonebook.c
+++ week3/phonebook.c
@@ -8,17 +8,17 @@ int main(void) //int main its good since we don't need command line arguments
     string names[] = {"Carter", "David"};   //create an array of strings "names"
     string numbers[] = {"617-555-0100", "617-555-0101"};   //create an array of "strings numbers" that relates to the names
 
-    string name = get_string("Name: ");
-    for (int i = 0; i < 2; i++)
+    string name = get_string("Name: ");  //get the name from the user
+    for (int i = 0; i < 2; i++) //loop through the names array
     {
-        if (strcmp(names [i], name) == 0)
+        if (strcmp(names [i], name) == 0) //if the given name is equal to the name in the array
         {
-            printf("Found %s\n", numbers [i]);
+            printf("Found %s\n", numbers [i]);  //print the number that corresponds to the name
             return 0;
 
         }
     }
-    printf("Not found\n");
+    printf("Not found\n");  //if the name is not found
     return 1;
 
 }
