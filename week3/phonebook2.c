2a65c1e538f5f8e0941db32ad110d3bfe1e2bfb4
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-04-17 09:47:50 +0000

    Mon, Apr 17, 2023, 9:47 AM +00:00

--- week3/phonebook2.c
+++ week3/phonebook2.c
@@ -2,31 +2,31 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct
+typedef struct  //create a struct
 {
-    string name;
-    string number;
+    string name;    //create a string name
+    string number;      //create a string number
 }
-person;
+person;     //call the struct person
 
 
 int main(void)
 
 {
-    person people[2];   //create an array of
+    person people[2];   //create an array of people
 
-    people[0].name = "Carter";
+    people[0].name = "Carter";  //assign the name and number to the people 0
     people[0].number = "617-555-0100";
 
-    people[1].name = "David";
+    people[1].name = "David";           //assign the name and number to the people 1
     people[1].number = "617-555-0101";
 
     string name = get_string("Name: ");  //prompt the user for a name
     for (int i = 0; i < 2; i++)
     {
-        if (strcmp(people[i].name, name) == 0)
+        if (strcmp(people[i].name, name) == 0)  //if the given name is equal to the name in the array
         {
-            printf("Found %s\n", people[i].number);
+            printf("Found %s\n", people[i].number);     //print the number that corresponds to the name
             return 0;
 
         }
