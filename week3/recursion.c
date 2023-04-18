7bb7ba25c6728f276de03c57550b80026413a54c
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-04-18 09:18:48 +0000

    Tue, Apr 18, 2023, 9:18 AM +00:00

--- week3/recursion.c
+++ week3/recursion.c
@@ -1,10 +1,11 @@
-#include    <cs50.h>        //include the libraries
-#include    <stdio.h>       //include the libraries
+#include    <cs50.h>        //include the libraries of cs50
+#include    <stdio.h>       //include the libraries for get_int and printf
 
 void draw(int n);    //declare the function draw
 
 int main(void)      //main function
 {
+    int height = get_int("Height: ");   //prompt the user for a height
     draw(1);    //call the function draw
 
 }
