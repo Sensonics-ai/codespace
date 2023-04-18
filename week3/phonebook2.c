59696ca050296bb0af6f97929d6c25921699c4e3
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-04-18 07:44:30 +0000

    Tue, Apr 18, 2023, 7:44 AM +00:00

--- week3/phonebook2.c
+++ week3/phonebook2.c
@@ -13,7 +13,7 @@ person;     //call the struct person
 int main(void)
 
 {
-    person people[2];   //create an array of people of size 2
+    person people[3];   //create an array of people of size 3
 
     people[0].name = "Carter";  //assign the name and number to the people 0
     people[0].number = "617-555-0100";
@@ -21,8 +21,11 @@ int main(void)
     people[1].name = "David";           //assign the name and number to the people 1
     people[1].number = "617-555-0101";
 
+    people[2].name = "Oriol";           //assign the name and number to the people 2
+    people[2].number = "645-840466";        //this is my number
+
     string name = get_string("Name: ");  //prompt the user for a name
-    for (int i = 0; i < 2; i++) //loop through the people array
+    for (int i = 0; i < 3; i++) //loop through the people array
     {
         if (strcmp(people[i].name, name) == 0)  //if the given name is equal to the name in the array
         {
