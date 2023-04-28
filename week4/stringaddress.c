0dc6904b5832da3e7b6d7cc983f004c166d30ac1
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-04-28 12:02:16 +0000

    Fri, Apr 28, 2023, 12:02 PM +00:00

---
+++ week4/stringaddress.c
@@ -0,0 +1,9 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string s = "Hi!";   //s is a string, and it points to the address of the first character of the string
+    printf("%s\n", s);  //prints the string
+
+}
