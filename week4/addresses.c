380c9549658521fcb8ee9f4adffc9a6aa64ada8a
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-04-28 11:31:59 +0000

    Fri, Apr 28, 2023, 11:31 AM +00:00

--- week4/addresses.c
+++ week4/addresses.c
@@ -3,6 +3,6 @@
 int main(void)
 {
     int n = 50;
-    int *p = &n;
-    printf("%p\n", p);
+    int *p = &n;        // p is a pointer to an integer, and it points to the address of n
+    printf("%p\n", p);  // print the address of n
 }
