c98b75d3baa9f44b90711d7de9c29234737b2c5c
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-05-03 11:25:24 +0000

    Wed, May 3, 2023, 11:25 AM +00:00

--- week4/stringaddress.c
+++ week4/stringaddress.c
@@ -3,9 +3,8 @@
 int main(void)
 {
     char *s = "Uri";
-    printf("%p\n", s);
-    printf("%p\n", &s[0]);
-    printf("%p\n", &s[1]);
-    printf("%p\n", &s[2]);
-    printf("%p\n", &s[3]);
+    printf("%p\n", *s);
+    printf("%p\n", *(s+1));
+    printf("%p\n", *(s+2));
+    printf("%p\n", *(s+3));
 }
