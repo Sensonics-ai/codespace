9e252e1f1fa4ff3c68ad3450a8b87d7fde9b5e3f
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-05-04 08:08:34 +0000

    Thu, May 4, 2023, 8:08 AM +00:00

--- week4/copy.c
+++ week4/copy.c
@@ -1,1 +1,18 @@
-#include <c
+#include <cs50.h>       // string type
+#include <ctype.h>      // toupper
+#include <stdio.h>      // printf
+#include <string.h>     // strlen
+
+int main(void)
+{
+    string s = get_string("s: ");       // get a string from the user
+
+    string t = s;                // copy the string into another variable
+
+    t[0] = toupper(t[0]);        // capitalize the first letter in the copy
+
+    printf("s: %s\n", s);        // print the original
+    printf("t: %s\n", t);        // print the copy
+
+
+}
