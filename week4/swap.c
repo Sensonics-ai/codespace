8c357d7849600ef13b1b03362aace96ea9bcc65c
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-05-04 10:07:27 +0000

    Thu, May 4, 2023, 10:07 AM +00:00

--- week4/swap.c
+++ week4/swap.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 
-void swap(int a, int b);
+void swap(int a, int b);    // function prototype
 
-int main(void)
+int main(void)        // main function
 {
     int x = 1;
     int y = 2;
 
-    printf("x: %i, y: %i\n", x, y);
-    swap(x, y);
-    printf("x: %i, y: %i\n", x, y);
+    printf("x: %i, y: %i\n", x, y); // print x and y
+    swap(x, y);                    // swap x and y
+        printf("x: %i, y: %i\n", x, y); // print x and y
+    }
+
+void swap(int a, int b)   // function definition
+{
+    int tmp = a;
+    a = b;
+    b = tmp;
 }
 
-void swap(int a, int b)
