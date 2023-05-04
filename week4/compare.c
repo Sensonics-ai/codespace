f50d58f6b895c328e614df4edac8e9c0aa332e16
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-05-04 07:22:32 +0000

    Thu, May 4, 2023, 7:22 AM +00:00

--- week4/compare.c
+++ week4/compare.c
@@ -1,17 +1,19 @@
-#include <cs50.h>
-#include <stdio.h>
+#include <cs50.h>       // cs50.h is needed for get_string
+#include <stdio.h>      // stdio.h is needed for printf
+#include <string.h>     // string.h is needed for strcmp
 
-int main(void)
+int main(void)        // main function
 {
     string s = get_string("s: "); // s is a pointer to a string
     string t = get_string("t: ");   // t is a pointer to a string
-    if (s == t) // compare the addresses of the two strings
+
+    if (strcmp(s, t) == 0)  // strcmp compares the strings and returns 0 if they are the same
     {
-        printf("Same\n");   // if the addresses are the same, the strings are the same
+        printf("Same\n");   // strcmp returns 0 if the strings are the same
     }
     else
     {
-        printf("Different\n");  // if the addresses are different, the strings are different
+        printf("Different\n");  // strcmp returns a non-zero value if the strings are different
     }
 
 }
