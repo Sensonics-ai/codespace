e66cc6bcaca9dca218bf3055adf0253a99ceff65
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-05-04 08:39:25 +0000

    Thu, May 4, 2023, 8:39 AM +00:00

--- week4/copy.c
+++ week4/copy.c
@@ -11,7 +11,7 @@ int main(void)
     char *t = malloc(strlen(s) + 1);  // allocate memory for another string +1 for the null character
                                       //at the moment t is a pointer to a string of length 0
 
-  for (int i = 0, i < strlen(s); i <= n; i++) // copy string into memory
+  for (int i = 0, i < strlen(s) + 1; i++) // copy string into memory
     {
         t[i] = s[i];                      // copy each character from s to  t
     }
