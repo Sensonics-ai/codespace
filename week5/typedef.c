6461d72b16eae5aaa0480ef4bd1ca47272d869d8
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-05-12 07:19:07 +0000

    Fri, May 12, 2023, 7:19 AM +00:00

---
+++ week5/typedef.c
@@ -0,0 +1,8 @@
+typedef struct      // define a new type called stack
+
+{
+    person people [CAPACITY];   // array of max amount of people
+    int size;                // current number of people
+
+}
+stack;                // stack is a type of structure
