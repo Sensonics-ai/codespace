891ca581c821f43cbfaeadfc51df04bc19627145
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-05-12 09:41:15 +0000

    Fri, May 12, 2023, 9:41 AM +00:00

--- week5/list.c
+++ week5/list.c
@@ -3,10 +3,19 @@
 int main(void)
 {
     int *list = malloc(3 * sizeof(int)); // allocate memory for 3 integers
+    if (list == NULL) // if malloc returns NULL, exit
+    {
+        return 1;   // return 1 to indicate an error
+    }
 
     list[0] = 1; // store 1 in the first integer
     list[1] = 2; // store 2 in the second integer
     list[2] = 3; // store 3 in the third integer
+
+    for (int i = 0; i < 3; i++) // iterate over the array
+    {
+        printf("%i\n", list[i]); // print each integer
+    }
 
 
 }
