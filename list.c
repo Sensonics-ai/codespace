97c38212e6b432eb983b5325328f6f0d06057c54
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-05-12 07:42:13 +0000

    Fri, May 12, 2023, 7:42 AM +00:00

--- list.c
+++ list.c
@@ -1,15 +1,15 @@
-#include   <stdio.h>
+#include   <stdio.h>    // printf
 
 int main(void)
 {
-    int list [3];
-    list[0] = 1;
-    list[1] = 2;
-    list[2] = 3;
+    int list [3];   // array of 3 integers
+    list[0] = 1;    // set first element to 1
+    list[1] = 2;    // set second element to 2
+    list[2] = 3;    // set third element to 3
 
     for (int i = 0; i < 3; i++)
     {
-        printf("%i\n", list[i]);
+        printf("%i\n", list[i]);    // print each element
     }
 
 }
