da352d25e396d51f5bf523a7b2d6017a1fbe4a9f
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-05-25 12:21:31 +0000

    Thu, May 25, 2023, 12:21 PM +00:00

---
+++ week6/dictionary.py
@@ -0,0 +1,22 @@
+words = set()                       # create empty set for words
+
+
+def check(word):                    # check if word is in dictionary
+    if word.lower() in words:       # convert word to lowercase
+        return True                 # if word is in dictionary, return True
+    else:                           # else return False
+        return False
+
+def load(dictionary):               # load dictionary into memory
+    file = open(dictionary, "r")    # open dictionary file
+    for line in file:               # read each line in file
+        words.add(line.rstrip("\n"))    # add word to set
+    file.close()                    # close file
+    return True                     # return True if successful
+
+def size():
+    return len(words)
+
+
+def unload():
+    return True
