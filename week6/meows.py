695680a781b20175a3a3caa319419a7ea553a99c
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-05-31 13:21:48 +0000

    Wed, May 31, 2023, 1:21 PM +00:00

--- week6/meows.py
+++ week6/meows.py
@@ -1,10 +1,10 @@
-
-
 def main():     # main function definition
     for i in range(5):      # for loop
         meow()      # function call
 
+
     def meow():     # function definition
         print("meow")       # function body/name
+
 
     main()      # function call
