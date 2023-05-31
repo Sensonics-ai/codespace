7ccb7d9ecfc42c2ee0093001c04c56429f75671a
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-05-31 08:08:23 +0000

    Wed, May 31, 2023, 8:08 AM +00:00

---
+++ week6/calculator.py
@@ -0,0 +1,6 @@
+from cs50 import get_int            # Importing the get_int function from the cs50 library
+
+x = get_int("x: ")      # Using the get_int function to get the user's input and storing it in the variable x
+y = get_int("y: ")      # Using the get_int function to get the user's input and storing it in the variable y
+
+print(x + y)            # Printing the sum of x and y
