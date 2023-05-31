dbd42dee2160557f0e70b2738c105161302948f5
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-05-31 13:02:59 +0000

    Wed, May 31, 2023, 1:02 PM +00:00

--- week6/agree.py
+++ week6/agree.py
@@ -2,8 +2,8 @@
 
 s = input("Do you agree? ")     # input() is a function that takes a string as an argument and returns a string as a result
 
-if s in ["Y", "y", "Yes", "yes"]:   # if the user types in Y, y, Yes, or yes, then print Agreed
+if s.lower() in ["y", "yes"]:   # s.lower() is a method that returns a string in lowercase
     print("Agreed.")
 
-elif s in ["N", "n", "No", "no"]:   # if the user types in N, n, No, or no, then print Not agreed
+elif s.lower() in ["n", "no"]:  # s.lower() can be used in python to convert a string to lowercase
     print("Not agreed.")
