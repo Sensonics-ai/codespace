da5d33345f7902c55031bb59970f5abc33938d1f
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-05-31 08:54:00 +0000

    Wed, May 31, 2023, 8:54 AM +00:00

--- week6/agree.py
+++ week6/agree.py
@@ -5,5 +5,5 @@ s = input("Do you agree? ")     # input() is a function that takes a string as a
 if s == "Y" or s == "y":        # == is a comparison operator
     print("Agreed.")            # Indentation is important in python
 
-elif s == "N" or s == "n":      # elif is short for else if, note how we can write or as or
+elif s == "N" or s == "n":      # elif is short for else if, note how we can write "or" as or
     print("Not agreed.")        # elif is optional
