204c82cd0555f5dabbb557d9e5401ce7d2c9549d
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-05-31 08:40:42 +0000

    Wed, May 31, 2023, 8:40 AM +00:00

--- week6/calculator.py
+++ week6/calculator.py
@@ -1,8 +1,8 @@
 
-x = input("x: ")       
+x = input("x: ")
 y = input("y: ")
 
 
 z = int(x) / int (y)          # / is a division operator
 
-print(f"{z:.50}")             # :.50 is a format specifier that tells python to print 50 decimal places
+print(f"{z:.50}")             # :.50 is a format specifier that tells python to print 50 decimal placesco
