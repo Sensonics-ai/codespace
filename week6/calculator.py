f2538cd73381d338b36aae41f516f2ace864c69c
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-05-31 08:18:25 +0000

    Wed, May 31, 2023, 8:18 AM +00:00

--- week6/calculator.py
+++ week6/calculator.py
@@ -2,4 +2,4 @@
 x = input("x: ")       # using input to get a number from the user
 y = input("y: ")       # using input to get a number from the user
 
-print(x + y)         # This will print the two numbers as strings, not add them together because they are strings
+print(x + y)
