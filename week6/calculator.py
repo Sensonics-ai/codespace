e09afd24fbb5e17a5ec42110bad7fc00f7951274
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-05-31 08:15:40 +0000

    Wed, May 31, 2023, 8:15 AM +00:00

--- week6/calculator.py
+++ week6/calculator.py
@@ -1,6 +1,5 @@
-from cs50 import get_int            # Importing the get_int function from the cs50 library
 
-x = get_int("x: ")      # Using the get_int function to get the user's input and storing it in the variable x
-y = get_int("y: ")      # Using the get_int function to get the user's input and storing it in the variable y
+x = input("x: ")                        # This will print the two numbers as strings, not add them together
+y = input("y: ")                        # This will print the two numbers as strings, not add them together
 
-print(x + y)            # Printing the sum of x and y
+print(x + y)                             # This will print the two numbers as strings, not add them together
