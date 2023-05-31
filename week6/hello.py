8544a3a41c5aabfe47ae5dd58d81113fd60a12c4
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-05-31 07:13:30 +0000

    Wed, May 31, 2023, 7:13 AM +00:00

--- week6/hello.py
+++ week6/hello.py
@@ -1,5 +1,6 @@
 from cs50 import get_string                     # Importing the get_string function from the cs50 library
 
-answer = get_string("What's your name? ")       # Using the get_string function to get the user's name
+answer = get_string("What's your name? ")       # Using the get_string function to get the user's name 
 print(f"hello, {answer}")                       # Printing the user's name, and the f stands for format string
+
 
