535b95d702c225da25fe5b5d72c100738d1a1af3
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-06-01 09:36:56 +0000

    Thu, Jun 1, 2023, 9:36 AM +00:00

--- week6/uppercase.py
+++ week6/uppercase.py
@@ -3,3 +3,5 @@ print ("After: ", end="")       # print "After: " with nothing
 for c in before:                # for each character in the string
     print(c.upper(), end="")    # print the uppercase version of the character, end with nothing
 print()                         # print () to go to next line in Python
+
+#this function wil take a string and return the uppercase version of it
