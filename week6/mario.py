da8f2090efe07345e30b16e28e4f81306382a52f
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-06-01 07:16:30 +0000

    Thu, Jun 1, 2023, 7:16 AM +00:00

---
+++ week6/mario.py
@@ -0,0 +1,10 @@
+def main():     # main function definition
+    for i in range(5):      # for loop
+        mario()      # function call
+
+
+def mario():     # function definition
+   print("#")       # function body/name
+
+
+main()      # function call
