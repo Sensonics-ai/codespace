bbff8545fa28d37d329fdc83b93ca21270b8cc1c
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-06-01 07:28:15 +0000

    Thu, Jun 1, 2023, 7:28 AM +00:00

--- week6/mario.py
+++ week6/mario.py
@@ -1,15 +1,18 @@
 from cs50 import get_int
 
-def main():                             # main function definition
-    height = get_int("Height: ")        # get height from user
-    for i in range(height):             # for loop
-        print("#")                      # print hashes
+def main():
+    height = get_int("Height: ")
+    for i in range(height):
+        print("#")
 
 
-def get_height():                       # function definition
-    while True:                         # while loop
-        n = int(input("Height: "))       # get height from user
-        if n >= 1 and n <= 8:           # check if height is between 1 and 8
-            return n                    # return height
+def get_height():
+    while True
+    Try:
+        n = int(input("Height: "))
+        if n >= 1 and n <= 8:
+            return n
+        except ValueError:
+            print("Invalid input")
 
-main()                              # function call
+main()
