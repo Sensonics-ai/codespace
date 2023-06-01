7fadd20b94171e9d3621d63f21ab07bb5b5c808e
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-06-01 07:59:35 +0000

    Thu, Jun 1, 2023, 7:59 AM +00:00

--- week6/mario.py
+++ week6/mario.py
@@ -1,18 +1,3 @@
-from cs50 import get_int
-
-def main():
-    height = get_height()
-    for i in range(height):
-        print("#")
-
-
-def get_height():
-    while True:
-     try:                                   # try to get an integer from the user
-        n = int(input("Height: "))          # if successful, return the integer
-        if n >= 1:                          # if the integer is equal or greater than 1
-            return n                        # return the integer
-     except ValueError:                      # if the user did not input an integer
-            print("Invalid input")          # print an error message
-
-main()
+for i in range(5):          # for loop
+    print("#", end="")      # print a hash
+print()                     # go to new line, because \n is default in python
