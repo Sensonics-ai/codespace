7f9a57bad7f7bb3cc057b6443683369d9170bdcf
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-06-01 09:40:32 +0000

    Thu, Jun 1, 2023, 9:40 AM +00:00

--- week6/uppercase.py
+++ week6/uppercase.py
@@ -1,7 +1,6 @@
 before = input("Before: ")      # get a string from the user
 print ("After: ", end="")       # print "After: " with nothing
-for c in before:                # for each character in the string
-    print(c.upper(), end="")    # print the uppercase version of the character, end with nothing
-print()                         # print () to go to next line in Python
+after = before.upper()          # convert the string to uppercase
+print(after)                    # print the uppercase string
 
-#this function wil take a string and return the uppercase version of it
+# # #this function will print the string in uppercase
