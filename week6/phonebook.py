ae86fb7517a1a3278e55031486458d410672c00c
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-06-01 11:14:45 +0000

    Thu, Jun 1, 2023, 11:14 AM +00:00

--- week6/phonebook.py
+++ week6/phonebook.py
@@ -1,12 +1,11 @@
 import csv                                   # imports the csv module for its functionality
 
-file = open("phonebook.csv", "a")            # opens the file in append mode
+with file = open("phonebook.csv", "a")  as file:          # "with" statement opens the file and closes it when done
 
-name = input("Name: ")                       # gets the name from the user
-number = input("Number: ")                   # gets the number from the user
+    name = input("Name: ")                       # gets the name from the user
+    number = input("Number: ")                   # gets the number from the user
 
-writer = csv.writer(file)                    # creates a writer object
+    writer = csv.writer(file)                    # creates a writer object
 
-writer.writerow([name, number])              # writes the name and number to the file
+    writer.writerow([name, number])              # writes the name and number to the file
 
-file.close()                                 # closes the file
