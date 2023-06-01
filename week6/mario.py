7bf2a81df2214bb76e9ed8b6a1a8b0aa936de8b7
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-06-01 07:20:30 +0000

    Thu, Jun 1, 2023, 7:20 AM +00:00

--- week6/mario.py
+++ week6/mario.py
@@ -1,5 +1,13 @@
 from cs50 import get_int
 
-while True:
-    n = get_int("Height: ")
-    if n >= 1 and n <= 8:
+def main():
+    height = get_int("Height: ")
+    for i in range(height):
+        print("#"):
+
+
+def get_height():
+    while True:
+        n = get_int("Height: ")
+        if n >= 1 and n <= 8:
+            return n
