0794ced65f0e333e3ec386b45e6eaeb260179cdd
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-06-01 11:11:18 +0000

    Thu, Jun 1, 2023, 11:11 AM +00:00

--- week6/phonebook.py
+++ week6/phonebook.py
@@ -1,19 +1,12 @@
-people = {  # Dictionary
-    "John": "123-456-7890",         # Key: Value
-    "Jane": "234-567-8901",
-    "Jerome": "345-678-9012",
-    "Jasmine": "456-789-0123",
-    "Jared": "567-890-1234",
-    "Jocelyn": "678-901-2345",
-    "Carter:": "789-012-3456",
+import csv                                   # imports the csv module for its functionality
 
-    #A dictionary is very similar to a list, or arrays in C, but instead of being limited to numbers, you can use words as the index
+file = open("phonebook.csv", "a")            # opens the file in append mode
 
-}
+name = input("Name: ")                       # gets the name from the user
+number = input("Number: ")                   # gets the number from the user
 
-name = input("Name: ")              # Get name from user
-if name in people:                 # If name is in people
-    number = people[name]          # Get number from people
-    print(f"Number: {number}")    # Print number
+writer = csv.writer(file)                    # creates a writer object
 
-    #this function will store names and numbers in a dictionary and print the number if the name is found in the dictionary
+writer.writerow([name, number])              # writes the name and number to the file
+
+file.close()                                 # closes the file
