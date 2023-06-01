ba4a06ff1ca6de6b2a28dcfda1b6d59bf20ecdc0
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-06-01 07:35:16 +0000

    Thu, Jun 1, 2023, 7:35 AM +00:00

--- week6/mario.py
+++ week6/mario.py
@@ -10,7 +10,7 @@ def get_height():
     while True:
      try:                                   # try to get an integer from the user
         n = int(input("Height: "))          # if successful, return the integer
-        if n >= 1                           # if the integer is equal or greater than 1
+        if n >= 1:                          # if the integer is equal or greater than 1
             return n                        # return the integer
      except ValueError:                      # if the user did not input an integer
             print("Invalid input")          # print an error message
