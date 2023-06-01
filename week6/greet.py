b6c42e53896c81c57c77e85f90b0133ca4d4fc91
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-06-01 10:18:34 +0000

    Thu, Jun 1, 2023, 10:18 AM +00:00

--- week6/greet.py
+++ week6/greet.py
@@ -1,6 +1,6 @@
 from sys import argv        # Import argv from sys module
 
-if len(argv) == 2:          # If there are 2 command-line arguments
+if len(argv) == 2:          # If there are 2 command-line arguments (the first is the program name)
     print(f"Hello, {argv[1]}")  # Print the second argument
 else:                       # Otherwise
     print("Hello, world")    # Print "Hello, world"
