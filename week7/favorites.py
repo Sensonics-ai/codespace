c4901c667ec32ba6dea6a8efeaf305ffe5747eaa
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-06-12 10:55:10 +0000

    Mon, Jun 12, 2023, 10:55 AM +00:00

--- week7/favorites.py
+++ week7/favorites.py
@@ -3,4 +3,5 @@ import csv                                  # import the csv module
 with open('favorites.csv', 'r') as f:       # open the csv file in python in read ("r") mode, and name it "f"
     reader = csv.reader(f)                  # load this file into the csv reader object (object is one of the packages in python)
     for row in reader:                      # iterate over the rows, for every row in the file print a different number
-        print(row[1])                       # print the second column of every row (the first column is row[0], the second is row[1], the 3rd is row[2], etc.)
+        favorite = row[1]                   # store the second column of every row in a variable called "favorite", which is the name of that column in the csv file
+        print(favorite)                       # print the column named "favorite" of every row, which we know is column 2
