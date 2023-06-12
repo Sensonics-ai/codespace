c5e9fb184eb5b0ef120dd2b78c458eec31eadded
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-06-12 12:06:34 +0000

    Mon, Jun 12, 2023, 12:06 PM +00:00

--- week7/countfavorites.py
+++ week7/countfavorites.py
@@ -1,16 +1,15 @@
 import csv                                  # import the csv module
 
-with open('favorites.csv', 'r') as f:           # open the csv file in python in read ("r") mode, and name it "f"
-    reader = csv.DictReader(f)                  # create a DictReader object called "reader" that will read the file "f" and store it in a dictionary
-    scratch, c, python = 0, 0, 0                # create three variables, one for each language, and set them all to 0
-    for row in reader:                          # iterate over the rows, for every row in the file print a different number
-        favorite = row["language"]              # store the value of the column named "language" in a variable called "favorite"
-        if favorite == "Scratch":               # if the value of the variable "favorite" is "Scratch"
-            scratch += 1                        # add 1 to the variable "scratch"
-        elif favorite == "C":                   # if the value of the variable "favorite" is "C"
-            c += 1                              # add 1 to the variable "c"
-        elif favorite == "Python":              # if the value of the variable "favorite" is "Python", we need to do elif  just in case someone adds a new language
-            python += 1                         # add 1 to the variable "python"
-print (f"Scratch: {scratch}")                   # print the variable "scratch"
-print (f"C: {c}")                               # print the variable "c"
-print (f"Python: {python}")                     # print the variable "python"
+with open('favorites.csv', 'r') as f:                  # open the csv file in python in read ("r") mode, and name it "f"
+    reader = csv.DictReader(f)                         # create a DictReader object called "reader" that will read the file "f" and store it in a dictionary
+    counts = {}                                        # create an empty dictionary called "counts"
+    for row in reader:                                 # iterate over the rows, for every row in the file print a different number
+        favorite = row["language"]                     # store the value of the column named "language" in a variable called "favorite"
+        counts[favorite] += 1                          # add 1 to the value of the key "favorite" in the dictionary "counts"
+        if favorite in counts:                          # if the key "favorite" is in the dictionary "counts"
+            counts[favorite] += 1                       # add 1 to the value of the key "favorite" in the dictionary "counts"
+        else:                                           # otherwise
+            counts[favorite] = 1                        # set the value of the key "favorite" in the dictionary "counts" to 1
+
+for each favorite in counts:                            # iterate over the keys in the dictionary "counts"
+    print(f"{favorite}: {counts[favorite]}")            # print the key and the value of the key in the dictionary "counts"
