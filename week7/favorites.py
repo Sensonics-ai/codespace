f5b2e1b2c7045593b7fe85a78a9c236b2d8e363b
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-06-14 09:54:31 +0000

    Wed, Jun 14, 2023, 9:54 AM +00:00

--- week7/favorites.py
+++ week7/favorites.py
@@ -1,6 +1,6 @@
-import csv                                  # import the csv module
+import csv
 
-with open('favorites.csv', 'r') as f:       # open the csv file in python in read ("r") mode, and name it "f"
-    reader = csv.reader(f)                  # load this file into the csv reader object (object is one of the packages in python)
-    for row in reader:                      # iterate over the rows, for every row in the file print a different number
-        print(row[2])                       # print the second column of every row (the first column is row[0], the second is row[1], the 3rd is row[2], etc.)
+with open('favorites.csv', 'r', encoding='utf-8') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print(row[2])
