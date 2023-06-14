30b0b17fd4bf72db643c55d913e98e5df90eb7bb
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-06-14 09:38:32 +0000

    Wed, Jun 14, 2023, 9:38 AM +00:00

--- week7/favorites.py
+++ week7/favorites.py
@@ -3,5 +3,5 @@ import csv                                  # import the csv module
 with open("favorites.csv", "r") as f:       # open the csv file in python in read ("r") mode, and name it "f"
     reader = csv.DictReader(f)              # create a DictReader object called "reader" that will read the file "f" and store it in a dictionary
     for row in reader:                      # iterate over the rows, for every row in the file print a different number
-        favorite = row["language"]          # store the value of the column named "language" in a variable called "favorite"
+        favorite = row["Grup"]          # store the value of the column named "language" in a variable called "favorite"
         print(favorite)                     # print the column named "favorite" of every row, which we know is column 2
