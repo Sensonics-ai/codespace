bc6466e9c8fa06e392ac79fdc0a9e1283ef0e543
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-07-19 11:15:39 +0000

    Wed, Jul 19, 2023, 11:15 AM +00:00

--- week9/hello/app.py
+++ week9/hello/app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)                                   # create an app instance
 
 @app.route("/")                                         # @ is a decorator, at the end point /
 def index():
-    if "name" in request.args:
-        name = request.args["name"]
-    else:
-          name = "World"
-    return render_template("index.html")
+    if "name" in request.args:                          # if there is a name in the request args
+        name = request.args["name"]                     # a variable called name with a value of the name in the request args
+    else:                                               # if there is no name in the request args
+          name = "World"                                # a variable called name with a value of "World"
+    return render_template("index.html", name=name)     # render the template with the name variable
 
