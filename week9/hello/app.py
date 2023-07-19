8fafc22985505184ca114306ea2fcb1ddc6df10f
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-07-19 10:32:45 +0000

    Wed, Jul 19, 2023, 10:32 AM +00:00

---
+++ week9/hello/app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, request       # import flask functions and libraries (the building blocks)
+
+app = Flask(__name__)                                   # create an app instance
+
+
+
+
+
