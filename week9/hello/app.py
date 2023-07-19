d05a8e61929863874136d0edb3dba33444eedaa5
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-07-19 11:04:16 +0000

    Wed, Jul 19, 2023, 11:04 AM +00:00

--- week9/hello/app.py
+++ week9/hello/app.py
@@ -5,5 +5,7 @@ app = Flask(__name__)                                   # create an app instance
 @app.route('/')                                         # @ is a decorator, at the end point /
 def index():
     return render_template('index.html')                 # which function to call
-
-
+    if "name" in request.args:
+    else:
+        return render_template('index.html')
+    app.run(debug=True)                                 # run the server in debug mode
