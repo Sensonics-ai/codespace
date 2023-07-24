59506cbb1a5882f52a75375dbc9529b612af5355
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-07-24 11:17:47 +0000

    Mon, Jul 24, 2023, 11:17 AM +00:00

--- week9/froshims/app.py
+++ week9/froshims/app.py
@@ -4,14 +4,18 @@ app = Flask(__name__)
 
 REGISTRANTS = {}
 
+SPORTS = ["Soccer", "Tennis", "Swimming", "Basketball", "Baseball", "Softball", "Football", "Volleyball", "Hockey"]
+
 @app.route("/")                                                             # route to /
 def index():                                                                # index() is a function
-    return render_template("index.html")                                    # render index.html
+    return render_template("index.html", sports=SPORTS)                                    # render index.html
 
 @app.route("/register", methods=["POST"])                                   # route to /register
 def register_user():                                                        # register_user() is a function
     name = request.form.get("name")                                         # get the name and sport from the form
-    sport = request.form.get("sport")                                       # get the name and sport from the form
+    sport = request.form.get("sport")                                        # get the name and sport from the form
+    if sport not in SPORTS:                                                 # if sport is not in SPORTS
+
     REGISTRANTS[name] = sport                                               # add name and sport to dictionary
     return render_template("success.html")                                  # render success.html
 
