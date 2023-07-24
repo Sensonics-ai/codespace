51e7df552515a573bdcd8e78a0193dd7f4f2ba92
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-07-24 10:47:17 +0000

    Mon, Jul 24, 2023, 10:47 AM +00:00

--- week9/froshims/app.py
+++ week9/froshims/app.py
@@ -4,17 +4,17 @@ app = Flask(__name__)
 
 REGISTRANTS = {}
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/")                                                             # route to /
+def index():                                                                # index() is a function
+    return render_template("index.html")                                    # render index.html
 
-@app.route("/register", methods=["POST"])
-def register_user():
-    name = request.form.get("name")
-    sport = request.form.get("sport")
-    REGISTRANTS[name] = sport
-    return render_template("success.html")
+@app.route("/register", methods=["POST"])                                   # route to /register
+def register_user():                                                        # register_user() is a function
+    name = request.form.get("name")                                         # get the name and sport from the form
+    sport = request.form.get("sport")                                       # get the name and sport from the form
+    REGISTRANTS[name] = sport                                               # add name and sport to dictionary
+    return render_template("success.html")                                  # render success.html
 
-@app.route("/registrants")
-def show_registrants():
-    return render_template("registrants.html", registrants=REGISTRANTS)
+@app.route("/registrants")                                                  # route to /registrants
+def show_registrants():                                                     # show_registrants() is a function
+    return render_template("registrants.html", registrants=REGISTRANTS)     # pass in the dictionary
