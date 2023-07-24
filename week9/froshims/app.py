105d4c50661019addc6800ea1d610dd160993ed5
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-07-24 10:01:20 +0000

    Mon, Jul 24, 2023, 10:01 AM +00:00

--- week9/froshims/app.py
+++ week9/froshims/app.py
@@ -3,6 +3,9 @@ from flask import Flask, render_template, request           # import flask funct
 app = Flask(__name__)                                       # create an app instance
 
 
+Registrants = []
+
+
 @app.route("/")                                             # @ is a decorator, at the end point /
 def index():                                                # define a function called index
     return render_template("index.html")                    # render the template
