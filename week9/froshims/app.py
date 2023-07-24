f16462a84c899c3f84903a83c5c7b7601079939c
Sensonics-ai <125382236+Sensonics-ai@users.noreply.github.com>
2023-07-24 09:37:16 +0000

    Mon, Jul 24, 2023, 9:37 AM +00:00

--- week9/froshims/app.py
+++ week9/froshims/app.py
@@ -7,10 +7,7 @@ app = Flask(__name__)                                       # create an app inst
 def index():                                                # define a function called index
     return render_template("index.html")                    # render the template
 
-@app.route("/register", methods=["POST"])
+@app.route("/register")
 def register():
-    name = request.form.get("name")
-    dorm = request.form.get("dorm")
-    if not name or not dorm:
-        return render_template("failure.html")
+
     return render_template("success.html")
